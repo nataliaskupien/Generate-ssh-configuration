cf6230d1fb899f3e13f1a36d0f2f695bc6559b7b
tomasz-github <31239749+tomasz-github@users.noreply.github.com>
2021-10-14 15:54:28 +0200

    Update gateway.cpp

--- gateway.cpp
+++ gateway.cpp
@@ -1,3 +1,3 @@
 #include "gateway.h"
 
-Gateway::Gateway(){}
+Gateway::Gateway(){} // Skoro klasa Gateway tylko trzyma dane to możesz plik gateway.cpp usunąć. Konstruktora w ogóle nie definiuj ani w cpp ani w h.
